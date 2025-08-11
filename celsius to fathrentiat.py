138e16a6efc78e4eecf124a2cd30ec20f20f0119
chittari515 <chittari.mahesh515@gmail.com>
2025-08-11 22:02:26 +0530

    Add files via upload

---
+++ celsius to fathrentiat.py
@@ -0,0 +1,3 @@
+celsius=float(input("enter the temperature in celsius:"))
+fah_temp=(celsius_temp * 9 /5) +32
+print("temperature n fahre heit:",fah_temp)
---
+++ logical opertation.py
@@ -0,0 +1,19 @@
+exp1=2>1
+exp2=5 < 4
+print("exp1 and  exp2:",exp1 and exp2)
+print("exp1 or exp2:",exp1 or exp2)
+print("not exp1:",not(exp1))
+x=5
+y=5
+print("if x is y:",x is y)
+print("if x is not y:",x is not y)
+# membership operators
+fruits=["apple","banana","cherry"]
+print("if banana is present in fruits:","banana" in fruits)
+print("if mango is not present in fruits:","mango"not in fruits)
+# bitwise operation
+a=10
+b=5
+print("a and b:", a & b)
+print("a or b:",a | b)
+print("a  xor b:", a ^ b)
